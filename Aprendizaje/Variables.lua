7e5425caea59c60859c34fe41f450ed0bbac80b9
Bubens <150555634+ElBubens@users.noreply.github.com>
2023-11-24 20:54:03 -0600

    Update Variables.lua

--- Aprendizaje/Variables.lua
+++ Aprendizaje/Variables.lua
@@ -1,1 +1,2 @@
---- Las variables son un espacio donde podemos almacenar dato, en especial hay 3 tipos, datos numericos, datos tipo texto y datos booleanos. Los booleanos son donde solo se puede guardar un true o un false
+--- Las variables son un espacio donde podemos almacenar dato, en especial hay 3 tipos, datos numericos, datos tipo texto y datos booleanos. 
+--- Los booleanos son donde solo se puede guardar un true o un false
