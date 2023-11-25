70ef3c5a47d9524127ee5711b2446404f1d701b6
Bubens <150555634+ElBubens@users.noreply.github.com>
2023-11-24 21:09:13 -0600

    Update Variables.lua
    completado

--- Aprendizaje/Variables.lua
+++ Aprendizaje/Variables.lua
@@ -1,2 +1,22 @@
 --- Las variables son un espacio donde podemos almacenar dato, en especial hay 3 tipos, datos numericos, datos tipo texto y datos booleanos. 
---- Los booleanos son donde solo se puede guardar un true o un false
+--- Los booleanos son donde solo se puede guardar un true o un false. Por ejemplo:
+
+nombre = "Buben" --- VARIABLE TIPO TEXTO
+edad = "999" --- VARIABLE NUMERICA
+suscriptores = 16 --- VARIABLE NUMERICA
+
+--- Ahora este es un ejemplo de unas pocas lineas de codigo para decir que Buben tiene 999 años y 16 suscriptores.
+
+print("Se llama", nombre, "su edad es", edad, "años, y tiene", suscriptores, "suscriptores")
+
+--- Este texto aparecera en la consola como:  "Se llama Buben su edad es 999 años, y tiene 16 suscriptores"
+
+--- Nota: Esta seccion del GitHub es solo APRENDIZAJE, no te recomiendo utilizar estos scripts a menos de que sea necesario,
+--- pero  si quieres utilizarlos, adelante! :D.  Si tienes alguna duda enviamela en los comentarios del canal.
+
+--██████╗ ██╗   ██╗       ██████╗ ██╗   ██╗██████╗ ███████╗███╗   ██╗███████╗
+--██╔══██╗╚██╗ ██╔╝██╗    ██╔══██╗██║   ██║██╔══██╗██╔════╝████╗  ██║██╔════╝
+--██████╔╝ ╚████╔╝ ╚═╝    ██████╔╝██║   ██║██████╔╝█████╗  ██╔██╗ ██║███████╗
+--██╔══██╗  ╚██╔╝  ██╗    ██╔══██╗██║   ██║██╔══██╗██╔══╝  ██║╚██╗██║╚════██║
+--██████╔╝   ██║   ╚═╝    ██████╔╝╚██████╔╝██████╔╝███████╗██║ ╚████║███████║
+--╚═════╝    ╚═╝          ╚═════╝  ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝
