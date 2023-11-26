14b60a50f8edd257228350f67b7c9d2b782f60b2
Bubens <150555634+ElBubens@users.noreply.github.com>
2023-11-26 16:17:27 -0600

    Create 10_RepeatUntil.lua

---
+++ Aprendizaje/10_RepeatUntil.lua
@@ -0,0 +1,25 @@
+--- En los scripts de Roblox, "Repeat Until" es una estructura de control que se utiliza para repetir un bloque de código hasta que se 
+--- cumpla una condición específica. A diferencia de otros bucles como "for" o "while", que se repiten mientras una condición 
+--- sea verdadera, el bucle "Repeat Until" se repite hasta que una condición sea verdadera. Ejemplo:
+
+edad = 0
+
+repeat
+	print("La edad es de", edad, "años.") --- Aca dira la edad.
+	edad += 1 --- Aca suma 1 a la edad.
+	wait(1) --- Indica que la accion se ejecute cada 1 segundo.
+until edad == 10 --- Aca indica que hasta que la edad sea igual a 10  se repetira el bucle.
+print("Ya cumplio su primera decada!") --- Aca como el valor de edad ya es 10 las demas lineas de codigo se ejecutaran.
+
+--- En el ejemplo anterior lo que hace es que se repite el bloque de código hasta que la variable "edad" sea igual a 10. 
+--- Cuando el valor llegue a 10, el bucle se detendrá y se imprimirá "Ya cumplio su primera decada!" en la consola. 
+
+--- Nota: Esta seccion del GitHub es solo APRENDIZAJE, no te recomiendo utilizar estos scripts a menos de que sea necesario,
+--- pero  si quieres utilizarlos, adelante! :D.  Si tienes alguna duda enviamela en los comentarios del canal.
+
+--██████╗ ██╗   ██╗       ██████╗ ██╗   ██╗██████╗ ███████╗███╗   ██╗███████╗
+--██╔══██╗╚██╗ ██╔╝██╗    ██╔══██╗██║   ██║██╔══██╗██╔════╝████╗  ██║██╔════╝
+--██████╔╝ ╚████╔╝ ╚═╝    ██████╔╝██║   ██║██████╔╝█████╗  ██╔██╗ ██║███████╗
+--██╔══██╗  ╚██╔╝  ██╗    ██╔══██╗██║   ██║██╔══██╗██╔══╝  ██║╚██╗██║╚════██║
+--██████╔╝   ██║   ╚═╝    ██████╔╝╚██████╔╝██████╔╝███████╗██║ ╚████║███████║
+--╚═════╝    ╚═╝          ╚═════╝  ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝
