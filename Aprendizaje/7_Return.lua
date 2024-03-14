f39cadfadad20c5a913adafeb0806f2090b2d2b4
Bubens <150555634+ElBubens@users.noreply.github.com>
2024-03-14 16:40:54 -0600

    Update 7_Return.lua

--- Aprendizaje/7_Return.lua
+++ Aprendizaje/7_Return.lua
@@ -1,9 +1,9 @@
---- En programación, la palabra clave return se utiliza para devolver un valor desde una función. 
---- Cuando una función alcanza una declaración return, se detiene la ejecución de la función y se devuelve el valor especificado.
---- El valor devuelto por una función puede ser utilizado en el contexto en el que se llamó a la función. 
---- Puede ser asignado a una variable, utilizado en una expresión o pasado como argumento a otra función.
+-- En programación, la palabra clave return se utiliza para devolver un valor desde una función. 
+-- Cuando una función alcanza una declaración return, se detiene la ejecución de la función y se devuelve el valor especificado.
+-- El valor devuelto por una función puede ser utilizado en el contexto en el que se llamó a la función. 
+-- Puede ser asignado a una variable, utilizado en una expresión o pasado como argumento a otra función.
 
---- Aquí tienes un ejemplo de una función que devuelve un valor:
+-- Aquí tienes un ejemplo de una función que devuelve un valor:
 
 function sumar(a, b)
 	local resultado = a + b
@@ -11,13 +11,13 @@ function sumar(a, b)
 end
 
 local resultado = sumar(3, 6)
-print("El resultado es:", resultado) --- En la consola mostrara el resultado es: 9
+print("El resultado es:", resultado) -- En la consola mostrara el resultado es: 9
 
---- En este ejemplo, la función sumar toma dos argumentos y devuelve su suma.
---- Return puede ser algo dificil de comprender  al principio, pero es una herramienta muy útil en la programación. 
+-- En este ejemplo, la función sumar toma dos argumentos y devuelve su suma.
+-- Return puede ser algo dificil de comprender  al principio, pero es una herramienta muy útil en la programación. 
 
---- Nota: Esta seccion del GitHub es solo APRENDIZAJE, no te recomiendo utilizar estos scripts a menos de que sea necesario,
---- pero  si quieres utilizarlos, adelante! :D.  Si tienes alguna duda enviamela en los comentarios del canal.
+-- Nota: Esta seccion del GitHub es solo APRENDIZAJE, no te recomiendo utilizar estos scripts a menos de que sea necesario,
+-- pero  si quieres utilizarlos, adelante! :D.  Si tienes alguna duda enviamela en los comentarios del canal.
 
 --██████╗ ██╗   ██╗       ██████╗ ██╗   ██╗██████╗ ███████╗███╗   ██╗███████╗
 --██╔══██╗╚██╗ ██╔╝██╗    ██╔══██╗██║   ██║██╔══██╗██╔════╝████╗  ██║██╔════╝
