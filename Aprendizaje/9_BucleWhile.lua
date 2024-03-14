52d07dc42c1cf30b84375cb561be5cf4e620f0c0
Bubens <150555634+ElBubens@users.noreply.github.com>
2024-03-14 16:42:00 -0600

    Update 9_BucleWhile.lua

--- Aprendizaje/9_BucleWhile.lua
+++ Aprendizaje/9_BucleWhile.lua
@@ -1,12 +1,12 @@
---- El bucle while es una estructura de control en programación que se utiliza para repetir un bloque de código mientras se cumpla una 
---- condición específica. El bucle while ejecuta el bloque de código repetidamente siempre que la condición sea evaluada como verdadera.
---- Por ejemplo:
+-- El bucle while es una estructura de control en programación que se utiliza para repetir un bloque de código mientras se cumpla una 
+-- condición específica. El bucle while ejecuta el bloque de código repetidamente siempre que la condición sea evaluada como verdadera.
+-- Por ejemplo:
 
---- La sintaxis basica del bucle while es la siguiente:
+-- La sintaxis basica del bucle while es la siguiente:
 
---- while "condicion" do
---- lineas de codigo a repetir
---- end
+-- while "condicion" do
+-- lineas de codigo a repetir
+-- end
 
 --- Aca un ejemplo
 
@@ -17,10 +17,10 @@ while contador <= 5 do
 	wait(1) --- Hacemos que la accion se ejecute cada 1 segundo
 end
 
---- En el anterior ejemplo  se utiliza el bucle while para mandar un mensaje a la consola del numero del contador.
---- Si el contador es 5 el codigo se dejara de ejecutar y ya no se sumara, pero si es menor, se seguira ejecutando.
+-- En el anterior ejemplo  se utiliza el bucle while para mandar un mensaje a la consola del numero del contador.
+-- Si el contador es 5 el codigo se dejara de ejecutar y ya no se sumara, pero si es menor, se seguira ejecutando.
 
---- Ahora otro ejemplo con creacion de instancias
+-- Ahora otro ejemplo con creacion de instancias
 
 while true do
 	local instancia = Instance.new("Part", workspace)
@@ -29,13 +29,13 @@ while true do
 	wait(1) --- Hacemos que la accion se ejecute cada 1 segundo
 end
 
---- En el anterior ejemplo se utiliza un bucle while para crear una parte cada un segundo  y se le asigna una posicion en el workspace.
+-- En el anterior ejemplo se utiliza un bucle while para crear una parte cada un segundo  y se le asigna una posicion en el workspace.
 
---- Los bucles whiles son de los temas mas sencillos de comprender y faciles de usar  en programacion, pero hay 
---- que tener cuidado con ellos y no arruinar  el rendimiento del juego.
+-- Los bucles whiles son de los temas mas sencillos de comprender y faciles de usar  en programacion, pero hay 
+-- que tener cuidado con ellos y no arruinar  el rendimiento del juego.
 
---- Nota: Esta seccion del GitHub es solo APRENDIZAJE, no te recomiendo utilizar estos scripts a menos de que sea necesario,
---- pero  si quieres utilizarlos, adelante! :D.  Si tienes alguna duda enviamela en los comentarios del canal.
+-- Nota: Esta seccion del GitHub es solo APRENDIZAJE, no te recomiendo utilizar estos scripts a menos de que sea necesario,
+-- pero  si quieres utilizarlos, adelante! :D.  Si tienes alguna duda enviamela en los comentarios del canal.
 
 --██████╗ ██╗   ██╗       ██████╗ ██╗   ██╗██████╗ ███████╗███╗   ██╗███████╗
 --██╔══██╗╚██╗ ██╔╝██╗    ██╔══██╗██║   ██║██╔══██╗██╔════╝████╗  ██║██╔════╝
