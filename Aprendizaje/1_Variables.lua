6a15c5f56b2ca6757dbad89d0167a53c8efbcbb5
Bubens <150555634+ElBubens@users.noreply.github.com>
2024-03-14 16:39:01 -0600

    Update 1_Variables.lua

--- Aprendizaje/1_Variables.lua
+++ Aprendizaje/1_Variables.lua
@@ -1,9 +1,9 @@
 -- Una variable es un espacio de memoria que se utiliza para almacenar y manipular datos. 
 -- Puede ser accedida y modificada desde cualquier parte del código, siempre y cuando esté en el mismo ámbito o alcance.
 
-nombre = "Buben" --- VARIABLE TIPO TEXTO
-edad = "999" --- VARIABLE NUMERICA
-suscriptores = 16 --- VARIABLE NUMERICA
+nombre = "Buben" -- VARIABLE TIPO TEXTO
+edad = "999" -- VARIABLE NUMERICA
+suscriptores = 16 -- VARIABLE NUMERICA
 
 -- Ahora este es un ejemplo de unas pocas lineas de codigo para decir que Buben tiene 999 años y 16 suscriptores.
 
