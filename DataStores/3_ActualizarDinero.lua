f73301a3a6fb267c372b490222b5ad828729c103
Bubens <150555634+ElBubens@users.noreply.github.com>
2024-03-14 16:45:15 -0600

    Update 3_ActualizarDinero.lua

--- DataStores/3_ActualizarDinero.lua
+++ DataStores/3_ActualizarDinero.lua
@@ -1,11 +1,11 @@
---- Definimos la funcion de que el dinero se le sume la cantidad de la ganancia ---
+-- Definimos la funcion de que el dinero se le sume la cantidad de la ganancia ---
 local function UpdateMoney()
 	dollars.Value =  dollars.Value + earnings.Value
 end
 
---- Hacemos que se sume cada segundo ---
+-- Hacemos que se sume cada segundo ---
 while true do
 	UpdateMoney()
-	wait(1) --- Si quieres cambiar cada cuantos segundos cambia el tiempo, en segundos
+	wait(1) -- Si quieres cambiar cada cuantos segundos cambia el tiempo, en segundos
 end
 
