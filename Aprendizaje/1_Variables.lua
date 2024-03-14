04b701e9ae7c77d4ca7b154f867c1a2184c1e4c0
Bubens <150555634+ElBubens@users.noreply.github.com>
2024-03-14 16:37:40 -0600

    Update 1_Variables.lua

--- Aprendizaje/1_Variables.lua
+++ Aprendizaje/1_Variables.lua
@@ -1,18 +1,18 @@
 -- Una variable es un espacio de memoria que se utiliza para almacenar y manipular datos. 
---- Puede ser accedida y modificada desde cualquier parte del código, siempre y cuando esté en el mismo ámbito o alcance.
+-- Puede ser accedida y modificada desde cualquier parte del código, siempre y cuando esté en el mismo ámbito o alcance.
 
 nombre = "Buben" --- VARIABLE TIPO TEXTO
 edad = "999" --- VARIABLE NUMERICA
 suscriptores = 16 --- VARIABLE NUMERICA
 
---- Ahora este es un ejemplo de unas pocas lineas de codigo para decir que Buben tiene 999 años y 16 suscriptores.
+-- Ahora este es un ejemplo de unas pocas lineas de codigo para decir que Buben tiene 999 años y 16 suscriptores.
 
 print("Se llama", nombre, "su edad es", edad, "años, y tiene", suscriptores, "suscriptores")
 
---- Este texto aparecera en la consola como:  "Se llama Buben su edad es 999 años, y tiene 16 suscriptores"
+-- Este texto aparecera en la consola como:  "Se llama Buben su edad es 999 años, y tiene 16 suscriptores"
 
---- Nota: Esta seccion del GitHub es solo APRENDIZAJE, no te recomiendo utilizar estos scripts a menos de que sea necesario,
---- pero  si quieres utilizarlos, adelante! :D.  Si tienes alguna duda enviamela en los comentarios del canal.
+-- Nota: Esta seccion del GitHub es solo APRENDIZAJE, no te recomiendo utilizar estos scripts a menos de que sea necesario,
+-- pero  si quieres utilizarlos, adelante! :D.  Si tienes alguna duda enviamela en los comentarios del canal.
 
 --██████╗ ██╗   ██╗       ██████╗ ██╗   ██╗██████╗ ███████╗███╗   ██╗███████╗
 --██╔══██╗╚██╗ ██╔╝██╗    ██╔══██╗██║   ██║██╔══██╗██╔════╝████╗  ██║██╔════╝
