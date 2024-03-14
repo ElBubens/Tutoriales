15b8147a8b9431a379e87a36aeab482104fc1953
Bubens <150555634+ElBubens@users.noreply.github.com>
2024-03-14 16:38:40 -0600

    Update 2_Condicionales.lua

--- Aprendizaje/2_Condicionales.lua
+++ Aprendizaje/2_Condicionales.lua
@@ -4,12 +4,12 @@
 numero1 = 7 --- Definimos nuestros valores.
 numero2 = 27
 
-if numero1 < numero2 then --- Empezamos con un if, si la condicion se cumple
-    print(numero1, "es menor que", numero2) --- Se ejecuta este codigo.
-elseif numero1 > numero2 then --- Si no se cumple la primera condicion y esta se cumple
-    print(numero2, "es menor que", numero1) --- Se ejecuta este codigo.
-elseif numero1 == numero2 then --- Si ninguna de las condiciones anteriores se cumplen
-    print(numero1, "y", numero2, "son iguales.") --- Se ejecuta este codigo.
+if numero1 < numero2 then -- Empezamos con un if, si la condicion se cumple
+    print(numero1, "es menor que", numero2) -- Se ejecuta este codigo.
+elseif numero1 > numero2 then -- Si no se cumple la primera condicion y esta se cumple
+    print(numero2, "es menor que", numero1) -- Se ejecuta este codigo.
+elseif numero1 == numero2 then -- Si ninguna de las condiciones anteriores se cumplen
+    print(numero1, "y", numero2, "son iguales.") -- Se ejecuta este codigo.
 end
 
 -- Todos los codigos que se ejecutarian es caso de las condiciones serian textos que aparecerian en la consola.
